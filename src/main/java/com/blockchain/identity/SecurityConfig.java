7dfe8f6f819c42bfb0882326bf6198f03cf276f7
vivekkatukam <katukamvivek@gmail.com>
2026-04-05 17:02:10 +0530

    Add backend code

--- src/main/java/com/blockchain/identity/SecurityConfig.java
+++ src/main/java/com/blockchain/identity/SecurityConfig.java
@@ -1,4 +1,4 @@
-package main.java.com.blockchain.identity;
+package com.blockchain.identity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
