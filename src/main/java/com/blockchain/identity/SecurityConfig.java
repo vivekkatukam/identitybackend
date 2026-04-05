f741584ced56c5e84ed862fbd41e7d9a2ce6710b
vivekkatukam <katukamvivek@gmail.com>
2026-04-05 16:52:28 +0530

    Fix security config package

--- src/main/java/com/blockchain/identity/SecurityConfig.java
+++ src/main/java/com/blockchain/identity/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.blockchain.identity;
+package main.java.com.blockchain.identity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
